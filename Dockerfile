9ecdabbdd40ea731123d1cc688079533a68cd22b
attalea <attaleastingray@mail.ru>
2026-07-31 00:47:13 +0000

    fix ci

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ FROM alpine:latest
 RUN apk --no-cache add ca-certificates
 WORKDIR /root/
 COPY --from=builder /task-manager .
-COPY migrations ./migrations
+COPY internal/db/migrations .internal/db/migrations
 EXPOSE 8080
 CMD ["./task-manager"]
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,6 +29,10 @@ func main() {
 	}
 	defer dbPool.Close()
 
+	if err := database.RunMigrations(cfg.DBUrl, "file://internal/db/migrations"); err != nil {
+		logger.Warn("failed to run migations (might be alppied)", "error", err)
+	}
+
 	natsBus, err := messaging.NewNATS(cfg.NATSUrl)
 	if err != nil {
 		logger.Error("nats error", "error", err)
